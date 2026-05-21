954603c33ce5d5bd8a492140d1e67d723367b766
Niranjan Nandam <niranjan.nandams99@gmail.com>
2026-05-21 16:55:36 +0530

    database setup

--- 02_Ecommerce_Customer_Analytics/sql/01_database_setup.sql
+++ 02_Ecommerce_Customer_Analytics/sql/01_database_setup.sql
@@ -1,1 +1,20 @@
+-- E-Commerce Customer Intelligence Project
 
+-- creating database
+CREATE DATABASE ecommerce_db;
+USE ecommerce_db;
+
+-- table create
+CREATE TABLE ecommerce (
+InvoiceNo VARCHAR(20),
+StockCode VARCHAR(20),
+Description VARCHAR(255),
+Quantity INT,
+InvoiceDate VARCHAR(50),
+UnitPrice DECIMAL(10,2),
+CustomerID VARCHAR(50),
+Country VARCHAR(50));
+
+-- indexing
+CREATE INDEX idx_customer_id ON ecommerce(CustomerID);
+CREATE INDEX idx_invoice_date ON ecommerce(InvoiceDate);
